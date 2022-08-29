4ce40f94881577336720c348c203537ed3cfee7d
Anubhav Chachra <chachraanubhav@gmail.com>
2022-08-29 20:44:39 +0530

    home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,7 +11,6 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Home Page"),
-
         ),
       body: Center(
           child: Container(
