9d97d74a40d9e5238d235eb13c89d6469c38ec92
Anubhav Chachra <chachraanubhav@gmail.com>
2022-09-09 22:08:21 +0530

    Day 9

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -6,45 +6,52 @@ class MyDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     // final imgUrl = "https://avatars.githubusercontent.com/u/71807967?v=4";
     return Drawer(
-      child : Container(
-
-        color: Colors.deepPurple,
-        child: ListView(
-        children: const [
-          DrawerHeader(
-            padding: EdgeInsets.zero,
-            child: UserAccountsDrawerHeader(
-                margin: EdgeInsets.zero,
-                accountName: Text("Anubhav Chachra"),
-                accountEmail: Text("chachraaanubhav@gmail.com"),
-                currentAccountPicture: CircleAvatar(
-                    backgroundImage: NetworkImage(
-                        "https://avatars.githubusercontent.com/u/71807967?v=4"))),
-          ),
-          ListTile(
-            leading: Icon(CupertinoIcons.home,
-            color: Colors.white,),
-            title: Text("Home",
-              style: TextStyle(
-              color: Colors.white,)),
-          ),
-          ListTile(
-            leading: Icon(CupertinoIcons.profile_circled,
-              color: Colors.white,),
-            title: Text("Profile",
-                style: TextStyle(
-                  color: Colors.white,)),
-          ),
-          ListTile(
-            leading: Icon(CupertinoIcons.mail,
-              color: Colors.white,),
-            title: Text("Email me",
-                style: TextStyle(
-                  color: Colors.white,)),
-          )
-        ],
-      )
-      ),
+      child: Container(
+          color: Colors.deepPurple,
+          child: ListView(
+            children: const [
+              DrawerHeader(
+                padding: EdgeInsets.zero,
+                child: UserAccountsDrawerHeader(
+                    margin: EdgeInsets.zero,
+                    accountName: Text("Anubhav Chachra"),
+                    accountEmail: Text("chachraaanubhav@gmail.com"),
+                    currentAccountPicture: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            "https://avatars.githubusercontent.com/u/71807967?v=4"))),
+              ),
+              ListTile(
+                leading: Icon(
+                  CupertinoIcons.home,
+                  color: Colors.white,
+                ),
+                title: Text("Home",
+                    style: TextStyle(
+                      color: Colors.white,
+                    )),
+              ),
+              ListTile(
+                leading: Icon(
+                  CupertinoIcons.profile_circled,
+                  color: Colors.white,
+                ),
+                title: Text("Profile",
+                    style: TextStyle(
+                      color: Colors.white,
+                    )),
+              ),
+              ListTile(
+                leading: Icon(
+                  CupertinoIcons.mail,
+                  color: Colors.white,
+                ),
+                title: Text("Email me",
+                    style: TextStyle(
+                      color: Colors.white,
+                    )),
+              )
+            ],
+          )),
     );
   }
 }
