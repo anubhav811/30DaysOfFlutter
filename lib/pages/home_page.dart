78149e99eb14f721bf4f7cf7ff71855db3c44870
Anubhav Chachra <chachraanubhav@gmail.com>
2022-09-15 23:42:05 +0530

    day 12 done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:fluttter_demo/models/catalog.dart';
 import 'package:fluttter_demo/widgets/drawer.dart';
-
+import 'dart:convert';
 import '../widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 30;
+
   final String name = "Anubhav";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson = await rootBundle
+        .loadString("assets/files/catalog.json")
+        .then((value) {});
+
+    final decodedData = jsonDecode(catalogJson);
+    var productData = decodedData["products"];
+    CatalogModel.items =
+        List.from(productData).map<Item>((item) => Item.fromMap(item)).toList();
+    setState(() {}); // rebuild the widget
+  }
 
   @override
   Widget build(BuildContext context) {
